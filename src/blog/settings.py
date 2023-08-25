3966c737c1dd0de7cbfe651be73f5c97690360eb
sackokalil <142978827+sackokalil@users.noreply.github.com>
2023-08-25 02:27:58 +0200

    Update settings.py

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'sacko',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
